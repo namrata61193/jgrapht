066034fd2852b841740087fa60ad5f0e46710456
namrata61193 <namrata.61193@gmail.com>
2015-03-17 16:45:46 -0500

    new script

---
+++ pullRunJenkins.sh
@@ -0,0 +1,4 @@
+git add *
+git commit -m "commit changes"
+git push origin master
+curl http://http://cs498dm-33a.cs.illinois.edu:8080/jenkins/git/notifyCommit?url=https://github.com/namrata61193/jgrapht.git
